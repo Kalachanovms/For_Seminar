14b821f2e162b0c33491336989c03e000d2f5650
Kalachanovms@gmail.com <>
2022-11-29 19:17:43 +0300

    added task 3

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,12 +1,15 @@
 ﻿Console.Clear();
+
 int DataEntry(string str)
 {
     Console.WriteLine(str);
     int line = int.Parse(Console.ReadLine());
     return line;
 }
+
 void FindQuarter(int ch)
 {
+
     if (ch == 1) Console.WriteLine (" x > 0 and y > 0");
     if (ch == 2) Console.WriteLine (" x < 0 and y > 0");
     if (ch == 3) Console.WriteLine (" x < 0 and y < 0");
@@ -14,4 +17,5 @@ void FindQuarter(int ch)
     
 }
 int ch = DataEntry("Введите четверть = ");
+
 FindQuarter(ch);
---
+++ Task03/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.WriteLine(str);
+    int line = int.Parse(Console.ReadLine());
+    return line;
+}
+
+double CalcDistance(int x, int y, int x1, int y1)
+{
+
+    double result = Math.Sqrt(Math.Pow(x1- x,2) + Math.Pow(y1 - y,2));
+    return result;
+}
+
+int x = DataEntry("Введите координаты X : = ");
+int y = DataEntry("Введите координаты Y = ");
+
+int x1 = DataEntry("Введите координаты X1 = ");
+int y1 = DataEntry("Введите координаты Y1 = ");
+
+double distance = CalcDistance(x, y, x1, y1);
+Console.WriteLine(distance);
