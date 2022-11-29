296e35fdb6183af843791c41d849f27b298ef735
Kalachanovms@gmail.com <>
2022-11-29 14:37:14 +0300

    Пример

---
+++ Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int DataEntryXY(string str)
+{
+    Console.WriteLine(str);
+    int line = int.Parse(Console.ReadLine());
+    return line;
+}
+void FindQandrat( int x, int y)
+{
+    if ((x > 0) && ( y > 0 )) Console.WriteLine("This first qandrat");
+    if ((x < 0) && ( y > 0 )) Console.WriteLine("This second qandrat");
+    if ((x < 0) && ( y < 0 )) Console.WriteLine("This third qandrat");
+    if ((x > 0) && ( y < 0 )) Console.WriteLine("This four qandrat");
+}
+
+int x = DataEntryXY("write find x = ");
+// Console.WriteLine(x);
+int y = DataEntryXY("write find y = ");
+// Console.WriteLine(y);
+FindQandrat(x,y);
