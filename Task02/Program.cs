655e920ab622021780a3aee950947c28aa3f2ce2
Kalachanovms@gmail.com <>
2022-11-29 15:25:53 +0300

    task02

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int DataEntry(string str)
+{
+    Console.WriteLine(str);
+    int line = int.Parse(Console.ReadLine());
+    return line;
+}
+void FindQuarter(int ch)
+{
+    if (ch == 1) Console.WriteLine (" x > 0 and y > 0");
+    if (ch == 2) Console.WriteLine (" x < 0 and y > 0");
+    if (ch == 3) Console.WriteLine (" x < 0 and y < 0");
+    if (ch == 4) Console.WriteLine (" x > 0 and y < 0");
+    
+}
+int ch = DataEntry("Введите четверть = ");
+FindQuarter(ch);
