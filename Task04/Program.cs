96044f5b989839e4fc1a1148717fef0fb3bb12bd
Kalachanovms@gmail.com <>
2022-11-29 20:30:23 +0300

    task4

---
+++ Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+void Tablesquare(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    int count = 1;
+    while ( count <= number)
+    {
+        int pow = count*count;
+        Console.Write(pow + " ");
+        count ++;
+    
+    }
+        Console.WriteLine();
+}
+
+Tablesquare("Write number N : ");
