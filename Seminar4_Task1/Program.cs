e1d6b1e27cd796768b099a072623ca74e2940c10
Kalachanovms@gmail.com <>
2022-12-04 08:00:32 +0300

    three task

---
+++ Seminar4_Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+// Метод - получить число;
+
+int FindSum(int number)
+{
+    int sum = 0;
+
+    for(int i = 1; i <= number; i++)
+    {
+    sum = sum + i;
+    }
+    return sum;
+}
+
+int number = DataEntry("Write you number : ");
+int result = FindSum(number);
+
+Console.WriteLine("Сумма всех чисел до А = " + result);
---
+++ Seminar4_Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе 
+// 456 -> 3
+// 78 - >
+// 89126 -> 5
+
+Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.WriteLine(str);
+    int numberOne = int.Parse(Console.ReadLine());
+    return numberOne;
+}
+
+int Digits(int number)
+{
+    int i = 0;
+
+    for (i = 0; number > 0; i++)
+    {
+        number = number / 10;
+    }
+    return i;
+}
+
+int FiveUnitNumber = DataEntry("Write number: ");
+int result = Digits(FiveUnitNumber);
+Console.WriteLine("Количество цифр в числе : " + result);
+
+
+
