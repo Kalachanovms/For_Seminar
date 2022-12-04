a8ef8c1f4b8a36706633224bbdbc45da838ac6e8
Kalachanovms@gmail.com <>
2022-12-04 14:33:52 +0300

    four task

---
+++ Seminar4_Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+int DataInput(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+
+}
+
+int[] FillArray(int[]array)
+{
+    int length = array.Length;
+    int index = 0;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int i;
+
+    for (i = 0; i < count; i++)
+    {
+        Console.Write(col[i] + " ");
+    }
+}
+
+int NumberN = DataInput("Введите число : ");
+int[] array = new int [NumberN];
+array = FillArray(array);
+PrintArray(array);
