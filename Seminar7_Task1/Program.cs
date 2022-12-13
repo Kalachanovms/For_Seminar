cc3ae4511e041c72d03ee0233b855bcb590e189b
Kalachanovms@gmail.com <>
2022-12-13 20:47:33 +0300

    Seminar_7

---
+++ Seminar7_Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]}");
+            
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+// 1. Получить от пользователя M/N
+int[,] CreateRandomArray(int rows, int columns, int leftrange, int rirghtrange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftrange, rirghtrange + 1);
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+int m = EnterNumber("Введите значение m : ");
+int n = EnterNumber("Введите значение n : ");
+
+// 2. Создаем двухмерный массив и заполняем его
+
+int[,] matrix = CreateRandomArray(m, n, 0, 10);
+
+// 3. Вывести массив 
+
+PrintArray(matrix);
+
+
+
+
---
+++ Seminar7_Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($" {array[i,j]} ");
+            
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+// 1. Получить от пользователя M/N
+int[,] CreateRandomArray(int rows, int columns, int leftrange, int rirghtrange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+int m = EnterNumber("Введите значение m : ");
+int n = EnterNumber("Введите значение n : ");
+
+// 2. Создаем двухмерный массив и заполняем его
+
+int[,] matrix = CreateRandomArray(m, n, 0, 10);
+
+// 3. Вывести массив 
+
+PrintArray(matrix);
